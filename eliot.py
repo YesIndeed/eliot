645652273a22a7de839b5586b7cc98d22f9ba39f
Stephen Ngo <stephen@cs.utexas.edu>
2017-04-26 23:13:53 -0500

    Added exit conditions

--- eliot.py
+++ eliot.py
@@ -15,7 +15,7 @@ insecurity. But still, he'd like to hear what you think*.
     sure why either.
 '''
 
-CULT_WORDS = ['cult', 'demon', 'devil', 'dark ones']
+CULT_WORDS = ['cult', 'demon', 'devil', 'dark ones', ]
 
 class EliotBot:
     def __init__(self):
@@ -31,9 +31,18 @@ class EliotBot:
             self.phraseManagers = {state:PhraseManager('dialog/%s.tp' % state) for state in self.states}
 
     def handle_state(self, s):
+        if 'goodbye' in s.lower():
+            return 'end'
         # enter and then don't leave the meltdown state
         if self._state == 'meltdown' or self._heat >= 50:
             return 'meltdown'
+        elif self._state == 'cult':
+            ss = self.sia.polarity_scores(s)
+            interest = 100
+            # If you're positive enough and don't mention cults, return to smalltalk.
+            if ss['pos'] > 0.5 and ss['pos'] > ss['neg'] and not any(word in s.lower() for word in CULT_WORDS):
+                heat -= 10
+                return 'smalltalk'
 
         # loop over all the states
         # if there is a tag that coincides, switch to that state.
@@ -43,7 +52,9 @@ class EliotBot:
                self._interest = 70
                return state
         self._interest -= 5
-        if self._state == 'intro' or self._interest < 40 and random.random() > .5:
+
+        # If Eliot gets bored of conversation, he might default to making small talk.
+        if self._state == 'intro' or self._interest < 40 and random.random() > .8:
             return 'smalltalk'
         return self._state
 
@@ -51,23 +62,32 @@ class EliotBot:
         # analyze the sentiment of a sentence
         ss = self.sia.polarity_scores(s)
         self._heat += 10 * ss['neg']
-        self._heat += 5 * ss['neu']
+        if any(word in s for word in CULT_WORDS):
+            self._heat += 10
+            self._state = 'defensive'
         print('heat: %i' % self._heat) # debug
-        # TODO: railroad the user into meltdown convo when heat >= 50
 
     def get_phrase(self, state, s):
         return self.phraseManagers[state].get(s)
 
     def run(self):
         print(self.get_phrase(self._state,'debug'))
-        while(self._state != 'meltdown'):
+        while(self._state != 'meltdown' and self._state != 'end'):
             s = input(">> ")
             # Update internal state according to input.
             self.handle_heat(s)
             self._state = self.handle_state(s)
-            if self._state == 'meltdown':
+            if self._state == 'meltdown' or self._state == 'end':
                 continue
             print(self.get_phrase(self._state,s))
+
+        if self._state == 'end':
+            if self._heat < 30:
+                print('I\'m glad that we could have this conversation.')
+                return
+            else:
+                print('I\'ve had enough of this. Good day.')
+                return
 
         with open('dialog/meltdown.convo') as f:
             for line in f:
